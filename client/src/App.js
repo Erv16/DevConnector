41dbeb1e125d8a74258f3fc04fda00060abea794
Erv16 <epalani1@binghamton.edu>
2019-12-29 22:15:11 +0530

    Implemented a private route for dashboard which checks to see if the user is logged in, if so then loads the components else redirects to the login screen

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 // Redux
 // Connects React with Redux
@@ -35,7 +36,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register}></Route>
               <Route exact path='/login' component={Login}></Route>
-              <Route exact path='/dashboard' component={Dashboard}></Route>
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
